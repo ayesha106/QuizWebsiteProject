a45f37e2eb69f11824d14c5bf29b1e2d6816f9df
ayesha106 <72278537+ayesha106@users.noreply.github.com>
2021-11-26 17:09:21 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "In which year was the Python 3.0 version was developed?",
+    answer: "2008",
+    options: [
+      "2008",
+      "2000",
+      "2009",
+      "2005"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which lamnguage runs in Web Browser?",
+    answer: "Javascripts",
+    options: [
+      "Java",
+      "c",
+      "python",
+      "Javascripts"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What do we use to define a block of code in Python language?",
+    answer: "Identation",
+    options: [
+      "Brackets",
+      "key",
+      "Identation",
+      "None of these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following statements is correct for variable names in Python language?",
+    answer: "Unlimited length",
+    options: [
+      "All variabl names must begin with an underscore",
+      "The variable name length is a maximum of 2",
+      "Unlimited length",
+      "None of these"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is not a keyword in Python language?",
+    answer: "val",
+    options: [
+      "val",
+      "try",
+      "with",
+      "raise"
+    ]
+  },
+  
+];
